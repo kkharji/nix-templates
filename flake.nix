d32d5e5074937e4acc1f5e84aaad78957e6bc124
tami5 <kkharji@protonmail.com>
2022-02-25 10:30:51 +0300

    chore: more templates

--- flake.nix
+++ flake.nix
@@ -13,5 +13,27 @@
         description = "An over-engineered Hello World in C";
       };
 
+
+      nixos.container = {
+        path = ./nixos/container;
+        description = "A NixOS container running apache-httpd";
+      };
+
+      misc.hercules-ci = {
+        path = ./mix/hercules-ci;
+        description =
+          "An example for Hercules-CI, containing only the necessary attributes for adding to your project.";
+      };
+
+      pandoc.report = {
+        path = ./pandoc/xelatex;
+        description = "A report built with Pandoc, XeLaTex and a custom font";
+      };
+
+      go.hello = {
+        path = ./go/hello;
+        description = "A simple Go package";
+      };
+    };
   };
 }
